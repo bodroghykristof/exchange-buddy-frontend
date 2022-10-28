aff00f894b3d19ec33febdef55db65ef570bb037
Kristóf Bodroghy <bodroghykristof@gmail.com>
2022-10-28 12:32:39 +0200

    create 3 routes and render content accordingly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrokerBuddyComponent } from './broker-buddy/broker-buddy.component';
+import { CurrencyMapComponent } from './currency-map/currency-map.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path: "statistics", component: StatisticsComponent},
+   {path: "broker-buddy", component: BrokerBuddyComponent},
+   {path: "currency-map", component: CurrencyMapComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
