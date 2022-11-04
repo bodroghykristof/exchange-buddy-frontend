24fc412a235618b9987e267168a616cc50ebe07a
Kristóf Bodroghy <bodroghykristof@gmail.com>
2022-11-04 22:53:46 +0100

    display error message dynamically on server error

--- src/app/exchange-rate-panel/exchange-rate-panel.component.ts
+++ src/app/exchange-rate-panel/exchange-rate-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ExchangeRate } from '../services/exchange-rate-service/exchange-rate.model';
 import { ExchangeRateService } from '../services/exchange-rate-service/exchange-rate.service';
 
@@ -12,14 +13,22 @@ export class ExchangeRatePanelComponent implements OnInit {
   baseCurrency: string = "huf";
   currencies: string[] = ["eur", "usd", "gbp", "rub", "jpy", "chf"];
   exchangeRates: ExchangeRate[] = [];
+  errorMessage: string | null = null;
 
   constructor(private readonly exchangeRateService: ExchangeRateService) {
     this.exchangeRates = this.currencies.map(c => ({currencyOne: c, currencyTwo: this.baseCurrency}));
   }
 
   ngOnInit(): void {
-    this.exchangeRateService.getExchangeRatesByBase("huf", this.currencies)
-                            .subscribe((data: ExchangeRate[]) => this.exchangeRates =  data);
+    let subscription: Subscription = this.exchangeRateService.getExchangeRatesByBase("huf", this.currencies)
+    .subscribe({
+      next: (data) => {
+        this.exchangeRates = data;
+        this.errorMessage = null;
+        subscription.unsubscribe();
+      },
+      error: () => this.errorMessage = "Exchange rates cannot be loaded, because our server is temporarily unavailable."
+    });
   }
 
 }
