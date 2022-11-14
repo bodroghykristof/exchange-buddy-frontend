392be4a6b37341447f655daea8bde83e27b17537
Kristóf Bodroghy <bodroghykristof@gmail.com>
2022-11-14 17:18:11 +0100

    show placeholder when equal currencies are selected

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CURRENCIES, DEFAULT_CURRENCY_ONE, DEFAULT_CURRENCY_TWO } from '../constants/currencies';
 
 @Component({
@@ -6,23 +6,25 @@ import { CURRENCIES, DEFAULT_CURRENCY_ONE, DEFAULT_CURRENCY_TWO } from '../const
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.scss']
 })
-export class StatisticsComponent implements OnInit {
+export class StatisticsComponent {
 
   currencies: string[];
-  currencyOne: string;
-  currencyTwo: string;
+  currencyOne?: string;
+  currencyTwo?: string;
 
   constructor() { 
     this.currencies = CURRENCIES;
     this.currencyOne = DEFAULT_CURRENCY_ONE;
     this.currencyTwo = DEFAULT_CURRENCY_TWO;
   }
-  
-  ngOnInit(): void {
-  }
 
-  onCurrencyChange(event: any): void {
-    console.log(this.currencyOne + ", " + this.currencyTwo);
+  onCurrencyChange(event: any, order: number): void {
+    if (this.currencyOne === this.currencyTwo) {
+      if (order === 1) {
+        this.currencyTwo = undefined;
+      } else {
+        this.currencyOne = undefined;
+      }
+    }
   }
-
 }
