165cf974707a5b3f3648872bf517ab2da81a7900
Kristóf Bodroghy <bodroghykristof@gmail.com>
2022-11-14 16:51:08 +0100

    add onchange listener to selections

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CURRENCIES, DEFAULT_CURRENCY_ONE } from '../constants/currencies';
+import { CURRENCIES, DEFAULT_CURRENCY_ONE, DEFAULT_CURRENCY_TWO } from '../constants/currencies';
 
 @Component({
   selector: 'app-statistics',
@@ -9,14 +9,20 @@ import { CURRENCIES, DEFAULT_CURRENCY_ONE } from '../constants/currencies';
 export class StatisticsComponent implements OnInit {
 
   currencies: StatCurrency[];
-  currencyOne: string;
+  currencyOne: StatCurrency;
+  currencyTwo: StatCurrency;
 
   constructor() { 
     this.currencies = CURRENCIES.map(c => ({name: c}));
-    this.currencyOne = DEFAULT_CURRENCY_ONE;
-   }
-
+    this.currencyOne = {name: DEFAULT_CURRENCY_ONE};
+    this.currencyTwo = {name: DEFAULT_CURRENCY_TWO};
+  }
+  
   ngOnInit(): void {
+  }
+
+  onCurrencyChange(event: any): void {
+    console.log(this.currencyOne + ", " + this.currencyTwo);
   }
 
 }
