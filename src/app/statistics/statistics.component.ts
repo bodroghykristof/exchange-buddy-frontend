abff289e502d23a96ea7fdb762c85ad9ebf2a09e
Kristóf Bodroghy <bodroghykristof@gmail.com>
2022-11-16 10:14:33 +0100

    add dropdown with timeframes

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CURRENCIES, DEFAULT_CURRENCY_ONE, DEFAULT_CURRENCY_TWO } from '../constants/currencies';
 
 @Component({
@@ -6,16 +6,43 @@ import { CURRENCIES, DEFAULT_CURRENCY_ONE, DEFAULT_CURRENCY_TWO } from '../const
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.scss']
 })
-export class StatisticsComponent {
+export class StatisticsComponent implements OnInit {
 
   currencies: string[];
   currencyOne?: string;
   currencyTwo?: string;
 
+  timeFrames: TimeFrame[] = [];
+  timeFrame?: TimeFrame;
+
   constructor() { 
     this.currencies = CURRENCIES;
     this.currencyOne = DEFAULT_CURRENCY_ONE;
     this.currencyTwo = DEFAULT_CURRENCY_TWO;
+  }
+
+  ngOnInit(): void {
+    const currentDate: Date = new Date();
+    const lastWeekDate: Date = new Date();
+    lastWeekDate.setDate(lastWeekDate.getDate() - 7);
+    this.timeFrames.push({label: "Last week", from: lastWeekDate, to: currentDate});
+
+    const twoWeeksAgoDate: Date = new Date(lastWeekDate.getTime());
+    twoWeeksAgoDate.setDate(twoWeeksAgoDate.getDate() - 7);
+    this.timeFrames.push({label: "Last 2 weeks", from: twoWeeksAgoDate, to: currentDate});
+
+    const monthAgoDate: Date = new Date();
+    monthAgoDate.setMonth(monthAgoDate.getMonth() - 1);
+    this.timeFrames.push({label: "Last month", from: monthAgoDate, to: currentDate});
+
+    const sixMonthsAgoDate: Date = new Date();
+    sixMonthsAgoDate.setMonth(sixMonthsAgoDate.getMonth() - 6);
+    this.timeFrames.push({label: "Last 6 months", from: sixMonthsAgoDate, to: currentDate});
+
+    const yearAgoDate: Date = new Date();
+    yearAgoDate.setFullYear(yearAgoDate.getFullYear() - 1);
+    this.timeFrames.push({label: "Last year", from: yearAgoDate, to: currentDate});
+
   }
 
   validateSelection(order: number): void {
@@ -28,4 +55,14 @@ export class StatisticsComponent {
     }
   }
 
+  onTimeFrameChange() {
+    console.log(this.timeFrame);
+  }
+
+}
+
+interface TimeFrame {
+  label: string;
+  from?: Date;
+  to?: Date;
 }
