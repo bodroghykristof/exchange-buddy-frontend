779458133db21b1243a823f9dc9ffc9ef07409fd
Kristóf Bodroghy <bodroghykristof@gmail.com>
2022-12-02 12:43:59 +0100

    display real exchange rates fetched from back-end

--- src/app/rest-api/endpoints.ts
+++ src/app/rest-api/endpoints.ts
@@ -4,3 +4,4 @@ export const BASE_URL: string = environment.serverUrl;
 
 export const EXCHANGE_RATE_ENDPOINT: string = '/exchange_rates';
 export const EXCHANGE_RATE_SSE_ENDPOINT: string = '/exchange_rates/sse';
+export const EXCHANGE_RATE_HISTORY_ENDPOINT: string = '/exchange_rates/history';
--- src/app/services/exchange-rate-service/exchange-rate.service.ts
+++ src/app/services/exchange-rate-service/exchange-rate.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable, of } from 'rxjs';
 import { dateFormat } from 'src/app/commons/date-util';
-import { BASE_URL, EXCHANGE_RATE_ENDPOINT, EXCHANGE_RATE_SSE_ENDPOINT } from 'src/app/rest-api/endpoints';
+import { BASE_URL, EXCHANGE_RATE_ENDPOINT, EXCHANGE_RATE_HISTORY_ENDPOINT, EXCHANGE_RATE_SSE_ENDPOINT } from 'src/app/rest-api/endpoints';
 import { QueryParamBuilder } from 'src/app/rest-api/queryparam-builder';
 import { BASE_CURRENCY, CURRENCIES_QUERY_STRING_KEY, CURRENCY_ONE, CURRENCY_SEPARATOR_CHAR, CURRENCY_TWO, FROM, TO, URL_DATE_FORMAT } from 'src/app/rest-api/queryparam-constans';
 import { SseService } from '../sse-service/sse.service';
@@ -52,28 +52,11 @@ export class ExchangeRateService {
       .addParam(TO, dateFormat(to, URL_DATE_FORMAT))
       .build();
 
-    console.log(queryParams);
 
-    return of(
-      [
-        {id: 1000, currencyOne: currencyOne, currencyTwo: currencyTwo, exchangeRate: 405.6591, creat: new Date()},
-        {id: 1001, currencyOne: currencyOne, currencyTwo: currencyTwo, exchangeRate: 404.1242, creat: new Date(new Date().setDate(new Date().getDate() - 1))},
-        {id: 1002, currencyOne: currencyOne, currencyTwo: currencyTwo, exchangeRate: 403.1011, creat: new Date(new Date().setDate(new Date().getDate() - 2))},
-        {id: 1003, currencyOne: currencyOne, currencyTwo: currencyTwo, exchangeRate: 404.1673, creat: new Date(new Date().setDate(new Date().getDate() - 3))},
-        {id: 1004, currencyOne: currencyOne, currencyTwo: currencyTwo, exchangeRate: 409.4901, creat: new Date(new Date().setDate(new Date().getDate() - 4))},
-        {id: 1005, currencyOne: currencyOne, currencyTwo: currencyTwo, exchangeRate: 408.2301, creat: new Date(new Date().setDate(new Date().getDate() - 5))},
-        {id: 1006, currencyOne: currencyOne, currencyTwo: currencyTwo, exchangeRate: 410.3410, creat: new Date(new Date().setDate(new Date().getDate() - 6))},
-      ]);
+      console.log(BASE_URL + EXCHANGE_RATE_HISTORY_ENDPOINT + queryParams)
 
-    // TODO send API request
-    // let queryParams: string = new QueryParamBuilder()
-    //   .addParam(BASE_CURRENCY, base)
-    //   .addParam(CURRENCIES_QUERY_STRING_KEY, currencies.join(CURRENCY_SEPARATOR_CHAR))
-    //   .build();
-
-    // return this.http.get<ExchangeRate[]>(BASE_URL + EXCHANGE_RATE_ENDPOINT + queryParams);
+    return this.http.get<ExchangeRate[]>(BASE_URL + EXCHANGE_RATE_HISTORY_ENDPOINT + queryParams);
 
   }
-
 
 }
